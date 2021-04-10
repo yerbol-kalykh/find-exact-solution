8150883c510570e048b36d158512736637348a94
Yerbol Kalykhbergenov <yerbol.kalykhbergenov@exact.com>
2021-04-11 01:39:02 +0200

    Created initial layout page for Leaderboard

---
+++ FindExactSolution.Domain/Data/Entities/Team.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FindExactSolution.Domain.Data.Entities
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
